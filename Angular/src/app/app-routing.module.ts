206c718c4924f8aab3b3817a2260a91e28f7265f
Sufyan <69829443+sufyanzahri@users.noreply.github.com>
2024-04-06 14:28:29 +0800

    Add existing project

---
+++ Angular/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './auth/components/signup/signup.component';
+import { LoginComponent } from './auth/components/login/login.component';
+import { BookCarComponent } from './modules/customer/components/book-car/book-car.component';
+
+const routes: Routes = [
+  { path: "register", component: SignupComponent },
+  { path: "login", component: LoginComponent },
+  { path: "admin", loadChildren: () => import("./modules/admin/admin.module").then(m => m.AdminModule) },
+  { path: "customer", loadChildren: () => import("./modules/customer/customer.module").then(m => m.CustomerModule) }
+  // { path: "book-car", component: BookCarComponent }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Spring Boot/src/main/java/com/coderdot/RentACarSpringApplication.java
@@ -0,0 +1,13 @@
+package com.coderdot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentACarSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentACarSpringApplication.class, args);
+	}
+
+}
